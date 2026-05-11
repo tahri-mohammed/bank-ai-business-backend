9ac4bbfbbace452c41c7202c97c7d65e49a2f6ff
tahri-mohammed <tahri0981@gmail.com>
2026-05-11 10:46:10 +0100

    Initialize Spring Boot backend

---
+++ business-backend/src/test/java/com/bank/business_backend/BusinessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.business_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
